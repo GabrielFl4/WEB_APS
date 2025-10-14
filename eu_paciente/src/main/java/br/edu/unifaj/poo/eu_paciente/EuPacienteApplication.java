c4091a8c3893b16c06fb89332ef5aaabcf04800d
Paulo Cesar Magrin <magrinpaulocesar@gmail.com>
2025-10-13 21:21:58 -0300

    Atualizacao

---
+++ eu_paciente/src/main/java/br/edu/unifaj/poo/eu_paciente/EuPacienteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unifaj.poo.eu_paciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuPacienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuPacienteApplication.class, args);
+	}
+
+}
---
+++ eu_paciente/src/test/java/br/edu/unifaj/poo/eu_paciente/EuPacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unifaj.poo.eu_paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuPacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
